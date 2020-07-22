1cbb04e83996a83095ace28cc50ab2fa6913c847
hoiru-futabaya <hoiru.hutabaya@gmail.com>
2020-07-22 21:14:40 +0900

    登録と削除を作った

--- main.py
+++ main.py
@@ -1,15 +1,60 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python
 # coding: 'utf-8'
 
 import json
 import urllib.request
 import pprint
+import trackingmoreclass
+import os
+import io,sys,ast
 
-number = input('追跡番号を入力:')
-url = 'http://nanoappli.com/tracking/api/' + number + '.json'
-res = urllib.request.urlopen(url)
-data = json.loads(res.read().decode('utf-8'))
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-#最新のステータスを取得
-newest = next(iter(reversed(data['statusList'])))
-pprint.pprint(newest)
+tracker = trackingmoreclass.track
+result = ""
+
+with open('trklist') as f:
+	trklist = ast.literal_eval(f.read())
+
+#trackingmoreの登録業者辞書（名前: code）
+carrierjp ={'日本郵便': 'japan-post', '日通': 'nippon', 'クロネコヤマト': 'taqbin-jp', '佐川急便': 'sagawa'}
+
+#モード選択
+print('【登録 → 1　追跡 → 2　削除 → 3】')
+mode = input('>>')
+
+if mode == '1':
+	#登録画面
+	print('【追跡する荷物の登録】')
+	carrier = input('運送会社を入力: ')
+	number = input('伝票番号を入力: ')
+
+	#trackingmoreから情報取得
+	urlStr = ''
+	requestData ="{\"tracking_number\": \"" + number + "\",\"carrier_code\":\"" + carrierjp[carrier] + "\"}"
+	result = tracker.trackingmore(requestData, urlStr, "post")
+
+	#追跡中の荷物を辞書で持っておく（運送会社: 伝票番号）
+	trklist[number] = carrier
+	with open('trklist', mode='w') as f:
+		f.write(str(trklist))
+
+elif mode == '2':
+	#追跡リストを表示
+	print('準備中')
+
+elif mode == '3':
+	#削除対象を入力
+	print('【リストから荷物を削除】')
+	carrier = input('運送会社を入力: ')
+	number = input('伝票番号を入力: ')
+	urlStr = "/" + carrierjp[carrier] + "/" + number
+	requestData = ""
+	result = tracker.trackingmore(requestData, urlStr, "codeNumberDelete")
+
+	#辞書からも削除（運送会社: 伝票番号）
+	trklist.pop(number)
+	with open('trklist', mode='w') as f:
+		f.write(str(trklist))
+
+print(result)
